634a01285917675b3afd7a63da14e75de949b43e
Nikhil Kashyap <nikhilk@canarys.com>
2020-06-16 13:14:22 +0530

    made new chnages

---
+++ docfx/docfx_project/src/SampleAPIS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SampleAPIS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ docfx/docfx_project/src/SampleAPIS/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SampleAPIS.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
